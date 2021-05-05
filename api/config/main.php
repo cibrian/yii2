a68adbee4cb3dec041efb2b0917e77f6e3fee998
Ricardo Cibrián <ricardo.cibrian@plandomino.com>
2021-05-05 14:50:25 -0500

    added api folder for rest endpoints

---
+++ api/config/main.php
@@ -0,0 +1,66 @@
+<?php
+$params = array_merge(
+    require __DIR__ . '/../../common/config/params.php',
+    require __DIR__ . '/../../common/config/params-local.php',
+    require __DIR__ . '/params.php',
+    require __DIR__ . '/params-local.php'
+);
+
+return [
+    'id' => 'app-api',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'api\controllers',
+    'defaultRoute' => 'collection',
+    'components' => [
+        'request' => [
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ],
+        ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'session' => [
+            // this is the name of the session cookie used for login on the frontend
+            'name' => 'advanced-api',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'redis' => [
+            'class' => 'yii\redis\Connection',
+            'hostname' => 'redis',
+            'port' => 6379
+,            'database' => 0,
+        ],
+        'cache'         => [
+           'class' => 'yii\redis\Cache',
+           'redis' => 'redis',
+        ],
+
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                [
+                    'class' => 'yii\rest\UrlRule',
+                    'controller' => [
+                        'collection', 'auth'
+                    ]
+                ],
+            ],
+        ],
+    ],
+    'params' => $params,
+];
---
+++ api/controllers/CollectionController.php
@@ -0,0 +1,32 @@
+<?php
+namespace api\controllers;
+
+use common\models\Collection;
+use yii\filters\auth\CompositeAuth;
+use yii\filters\auth\HttpBearerAuth;
+use yii\rest\ActiveController;
+
+class CollectionController extends ActiveController
+{
+    public $modelClass = Collection::class;
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator'] = [
+            'class' => CompositeAuth::className(),
+            'authMethods' => [
+                HttpBearerAuth::className(),
+            ],
+        ];
+        return $behaviors;
+    }
+
+    public function actions(){
+        $actions = parent::actions();
+        unset($actions['create']);
+        unset($actions['update']);
+        unset($actions['delete']);
+        return $actions;
+    }
+}
