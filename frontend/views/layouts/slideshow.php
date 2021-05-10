7adf3e8c4469ed753a005e0ae4cc97f252bf2cf4
Ricardo Cibrián <ricardo.cibrian@plandomino.com>
2021-05-09 19:14:03 -0500

    improve stop/start button

--- frontend/views/layouts/slideshow.php
+++ frontend/views/layouts/slideshow.php
@@ -9,15 +9,18 @@
         <!-- slider -->
         <div class="slideshow-inner">
           <?php foreach ($collection->photos as $photo): ?>
-          <div class="mySlides ">
+          <div class="mySlides">
             <img src="<?= $photo->photo_path ?>" style='width: 100%;height: 100%' alt="sally lightfoot crab"/>
           </div>
           <?php endforeach; ?>
         </div>
+        <button type="button" class='btn btn-primary play' onclick="control()">
+        <i class="fa fa-play"></i> /
+        <i class="fa fa-stop"></i>
+      </button>
         <a class="prev" onclick='plusSlides(-1)'>&#10094;</a>
         <a class="next" onclick='plusSlides(1)'>&#10095;</a>
       </div>
-      <button class="btn btn-light" onclick="control()">Start/Stop</button>
       <div style='text-align: center; display: none'>
       <?php for ($i=1;$i<=count($collection->photos);$i++): ?>
         <span class="dot" onclick="currentSlide(<?= $i ?>)"></span>
@@ -27,3 +30,22 @@
       </div>
     </div>
 </div>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style type="text/css">
+
+
+.play {
+  position: absolute;
+  top: 90%;
+  left: 92%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  color: white;
+  font-size: 16px;
+  padding: 8px 18px;
+  border: none;
+  cursor: pointer;
+  border-radius: 5px;
+}
+
+</style>
